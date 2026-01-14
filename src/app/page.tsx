d2b095f5c34de43f950cb3269522be001a800d6d
emmanuel <emmanueloghene72@gmail.com>
2026-01-15 00:40:15 +0100

    Home redirext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,17 @@
-import React from "react";
+"use client";
 
-const page = () => {
-  return <div>Home Page</div>;
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Loader from "@/components/ui/Loader";
+
+const Page = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/login");
+  }, [router]);
+
+  return <Loader text="Loading..." />; // Or a loading spinner
 };
 
-export default page;
+export default Page;
